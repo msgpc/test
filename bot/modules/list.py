6cf37348c83926edc4834f28dc5afa78cf395727
Unathur <106528488+Unathur@users.noreply.github.com>
2022-06-06 11:26:33 +0530

    List

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -12,12 +12,12 @@ from bot.helper.telegram_helper import button_build
 def list_buttons(update, context):
     user_id = update.message.from_user.id
     if len(update.message.text.split(" ", maxsplit=1)) < 2:
-        return sendMessage('Send a search key along with command', context.bot, update.message)
+        return sendMessage('𝐒𝐞𝐧𝐝 𝐚 𝐬𝐞𝐚𝐫𝐜𝐡 𝐤𝐞𝐲 𝐚𝐥𝐨𝐧𝐠 𝐰𝐢𝐭𝐡 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 🙄', context.bot, update.message)
     buttons = button_build.ButtonMaker()
-    buttons.sbutton("Folders", f"types {user_id} folders")
-    buttons.sbutton("Files", f"types {user_id} files")
-    buttons.sbutton("Both", f"types {user_id} both")
-    buttons.sbutton("Cancel", f"types {user_id} cancel")
+    buttons.sbutton("ғᴏʟᴅᴇʀs", f"types {user_id} folders")
+    buttons.sbutton("ғɪʟᴇs", f"types {user_id} files")
+    buttons.sbutton("ʙᴏᴛʜ", f"types {user_id} both")
+    buttons.sbutton("ᴄᴀɴᴄᴇʟ", f"types {user_id} cancel")
     button = InlineKeyboardMarkup(buttons.build_menu(2))
     sendMarkup('Choose option to list.', context.bot, update.message, button)
 
@@ -32,10 +32,10 @@ def select_type(update, context):
         return query.answer(text="Not Yours!", show_alert=True)
     elif data[2] == 'cancel':
         query.answer()
-        return editMessage("list has been canceled!", msg)
+        return editMessage("𝐥𝐢𝐬𝐭 𝐡𝐚𝐬 𝐛𝐞𝐞𝐧 𝐜𝐚𝐧𝐜𝐞𝐥𝐞𝐝!👀", msg)
     query.answer()
     item_type = data[2]
-    editMessage(f"<b>Searching for <i>{key}</i></b>", msg)
+    editMessage(f"<b>𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠 𝐟𝐨𝐫 <i>{key}</i> 𝐏𝐥𝐞𝐚𝐬𝐞 𝐖𝐚𝐢𝐭...</b>", msg)
     Thread(target=_list_drive, args=(key, msg, item_type)).start()
 
 def _list_drive(key, bmsg, item_type):
@@ -45,7 +45,7 @@ def _list_drive(key, bmsg, item_type):
     if button:
         editMessage(msg, bmsg, button)
     else:
-        editMessage(f'No result found for <i>{key}</i>', bmsg)
+        editMessage(f'😐 𝐍𝐨 𝐫𝐞𝐬𝐮𝐥𝐭 𝐟𝐨𝐮𝐧𝐝 𝐟𝐨𝐫 <i>{key}</i>', bmsg)
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_buttons, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 list_type_handler = CallbackQueryHandler(select_type, pattern="types", run_async=True)
